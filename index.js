bee26205c66993fb9c41f8b83ad99affab19df57
Jason <jasontsemf@gmail.com>
2020-11-20 22:13:33 -0500

    updating front end structure

--- index.js
+++ index.js
@@ -4,15 +4,31 @@ const express = require('express');
 const app = express();
 
 const SerialPort = require('serialport');
-const port = new SerialPort('/dev/tty.usbmodem14101');
+const port = new SerialPort('/dev/tty.usbmodem14201');
 
 app.use(express.static('public'));
 app.use(express.json());
 
+
+// to static pages
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+app.get("/sign", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/sign.html"));
+});
+
+app.get("/receive", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/receive.html"));
+});
+
+app.get("/draw", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/draw.html"));
+});
+
+
+// actual API calls
 app.get("/move/:xy", async (req, res) => {
     let temp = req.params.xy.split(',');
     console.log(temp);
@@ -22,12 +38,8 @@ app.get("/move/:xy", async (req, res) => {
         if (err) {
             return console.log('Error on write: ', err.message)
         }
-        console.log('message written');
+        console.log('message written', x,y);
     })
-});
-
-app.get("/hi", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/hi.html"));
 });
 
 app.listen(8080, () => {
